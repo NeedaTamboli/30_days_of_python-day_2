cfd0d4fd3e230508be71334e70952a0355549b79
NeedaTamboli <114369431+NeedaTamboli@users.noreply.github.com>
2022-09-26 13:50:16 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+#'Day 2: 30 Days of python programming'
+first_name = "Needa"
+last_name = "Tamboli"
+full_name = "Needa Tamboli"
+country = "India"
+city = "Mumbai"
+age = 34
+year = 2022
+is_married ="Yes
+is_true = "Yes"
+is_light_on = "Yes"
+name_spouse, kids = "Shahanavaz", 3
